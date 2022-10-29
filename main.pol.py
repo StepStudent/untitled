be12208b98a6bb2e98e77a481257e248b24d6ca3
Студент <student@ITStep.local>
2022-10-29 13:35:10 +0300

    Initial commit

---
+++ main.pol.py
@@ -0,0 +1,9 @@
+    import random
+    class Human:
+        def__init__(self, neme="Humen", job=None, Home=None, car=None):
+        self.neme = neme
+        self.job = job
+        self.home = home
+        self.car = car
+        self.gladness = 50
+        self.stiety = 50
